d30a432a5d33b432ca0977f3f6a07de4f89011dc
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-25 15:44:34 +0900

    ✨ Passed 07_struct exercises

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -23,6 +23,12 @@ fn create_order_template() -> Order {
 
 fn main() {
     // You can optionally experiment here.
+    let order_template = create_order_template();
+    let my_order = Order {
+      name: String::from("Wooyeol Lee"),
+      ..order_template
+    };
+    println!("{:?}", my_order);
 }
 
 #[cfg(test)]
@@ -34,7 +40,11 @@ mod tests {
         let order_template = create_order_template();
 
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order {
+          name: "Hacker in Rust".to_string(),
+          count: 1,
+          ..order_template
+        };
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
